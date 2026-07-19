1a54053dc2de1ab13ac8526cf54bee0b3de4a6d6
PCarlosJr01 <pcarlosjr01@gmail.com>
2026-07-18 18:19:42 -0700

    fix: restore reliable sequential diffuser settings

--- settings.py
+++ settings.py
@@ -12,14 +12,7 @@ ON_SETTINGS = {
 }
 
 OFF_SETTINGS = {
-    1: False,
-    11: False,
-    12: 0,
-    13: "0",
-    14: 0,
     103: "off",
-    108: "00aaff00c86464",
-    109: "white",
-    110: "2",
-    111: 255,
+    11: False,
+    1: False,
 }
